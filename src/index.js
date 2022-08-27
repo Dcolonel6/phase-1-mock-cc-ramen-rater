0f7e65fc6c96660b7edfe847b6414dfc8477d8e4
frustrated-coder <d.n_mwangi@outlook.com>
2022-08-27 19:27:20 +0300

    finished all the core deliverables

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 // write your code here
 const form = document.querySelector('#new-ramen')
-const nameInput = form['new-name']
-const restaurantInput = form['new-restaurant']
-const imageInput = form['new-image']
-const ratingInput = form['new-rating']
-const commentInput = form['new-comment']
 const baseUrl = 'http://localhost:3000'
 const ramenMenu = document.querySelector('#ramen-menu')
 const ramenDetail = document.querySelector('#ramen-detail')
+const ratingDisplay = document.querySelector('#rating-display')
+const commentDisplay = document.querySelector('#comment-display')
+
 
 //on page loads, get all images and display them in #ramen-menu
 document.addEventListener('DOMContentLoaded',onPageLoad)
-
+form.addEventListener('submit',submitHandler)
 
 
 
@@ -30,7 +28,9 @@ async function onPageLoad(event){
         const imageEle = createElement('img',
         {
             src: img.image,
-            alt: `${img.restaurant}-${img.name}`
+            alt: `${img.restaurant}-${img.name}`,
+            'data-comment': img.comment,
+            'data-rating':img.rating
         })
        ramenMenu.append(imageEle) 
        imageEle.addEventListener('click',imageHandler)
@@ -46,7 +46,34 @@ function imageHandler(event){
     ramenDetail.querySelector('.name').textContent = nameTarget
     ramenDetail.querySelector('.restaurant').textContent = restaurantTarget
     ramenDetail.querySelector('img').setAttribute('src',srcTarget)
+    ratingDisplay.textContent = target.getAttribute('data-rating')
+    commentDisplay.textContent = target.getAttribute('data-comment')
+    
 
+}
+
+async function submitHandler(event){
+    //add image to ramen-menu then post it to our server
+    event.preventDefault()
+    const nameInput = form['new-name'].value
+    const restaurantInput = form['new-restaurant'].value
+    const imageInput = form['new-image'].value
+    const ratingInput = form['new-rating'].value
+    const commentInput = form['new-comment'].value
+
+    //create an image
+    const imageEle = createElement('img',{
+            src: imageInput,
+            alt: `${restaurantInput}-${nameInput}`,
+            'data-comment': commentInput,
+            'data-rating':ratingInput
+        })
+    ramenMenu.append(imageEle)
+  
+
+    target.reset()
+
+    console.log(response)
 
 }
 
@@ -64,17 +91,7 @@ function get(path){
     return fetch(baseUrl+path)
 }
 
-function post(url, payload){
-    const config  = {
-        method: 'post',
-        headers: {
-            accept: 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(payload)
-    }
 
-}
 
 function createElement(tag, attributes = {}) {
     const element = document.createElement(tag);
